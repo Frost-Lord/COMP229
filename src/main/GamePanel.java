cd6c77dbae18d60aac4875a9525e6e3420f10790
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-02-02 13:07:27 +1100

    Project loop update

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     final int originalTitleSize = 16;
     final int scale = 3;
@@ -14,10 +14,32 @@ public class GamePanel extends JPanel {
     final int screenWidth = titleSize * maxScreenCol;
     final int screenHeight = titleSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void run() {
+        while(gameThread != null) {
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
     }
 
 }
