0849d7c5aa26b328a01e7f9e3ad2128d49fa162b
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-02-03 13:27:57 +1100

    Load img update

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -37,41 +37,10 @@ public class TileManager {
                     "res/tile/water04.png"
             };
 
-            tile[10] = new Tile();
-            tile[10].image = ImageIO.read(new File(("res/tile/grass00.png")));
-
-            tile[11] = new Tile();
-            tile[11].image = ImageIO.read(new File(("res/tile/road00.png")));
-
-            tile[12] = new Tile();
-            tile[12].image = ImageIO.read(new File(("res/tile/water00.png")));
-
-            tile[13] = new Tile();
-            tile[13].image = ImageIO.read(new File(("res/tile/tree.png")));
-
-            tile[14] = new Tile();
-            tile[14].image = ImageIO.read(new File(("res/tile/sand.png")));
-
-            tile[15] = new Tile();
-            tile[15].image = ImageIO.read(new File(("res/tile/wall.png")));
-
-            tile[16] = new Tile();
-            tile[16].image = ImageIO.read(new File(("res/tile/wall.png")));
-
-            tile[17] = new Tile();
-            tile[17].image = ImageIO.read(new File(("res/tile/water02.png")));
-
-            tile[18] = new Tile();
-            tile[18].image = ImageIO.read(new File(("res/tile/water03.png")));
-
-            tile[19] = new Tile();
-            tile[19].image = ImageIO.read(new File(("res/tile/water05.png")));
-
-            tile[20] = new Tile();
-            tile[20].image = ImageIO.read(new File(("res/tile/water06.png")));
-
-            tile[21] = new Tile();
-            tile[21].image = ImageIO.read(new File(("res/tile/water04.png")));
+            for (int i = 0; i < imageNames.length; i++) {
+                tile[i + 10] = new Tile();
+                tile[i + 10].image = ImageIO.read(new File(imageNames[i]));
+            }
 
         }catch (IOException e) {
             e.printStackTrace();
